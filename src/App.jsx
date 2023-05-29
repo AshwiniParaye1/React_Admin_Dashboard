afc184e4de5464161b1e4b6db91b46b1054211a1
Ashwini <ashwiniparaye9309@gmail.com>
2023-05-29 19:12:29 +0530

    login style done

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import "./App.css";
 import RightSide from "./components/RightSideComponent/RightSide";
 import Sidebar from "./components/Sidebar/Sidebar";
 import MainDash from "./components/MainDash/MainDash";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <div className="AppGlass">
-        <Sidebar />
+    <>
+      <div className="App">
+        <div className="AppGlass">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Login />} />
+            </Routes>
+          </Router>
+          {/* <Sidebar />
         <MainDash />
-        <RightSide />
+        <RightSide /> */}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
